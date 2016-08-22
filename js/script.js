ffb7a00fa0b2bcac47178d0ef00954197885445d
Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
2016-08-22 15:25:02 +0200

    repair bug
    Remove last char from response

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ jQuery(function($) {
             //document.getElementById('title_dialog').innerHTML = title;
             var url = location.origin + ajaxurl;
             $.post(url, data, function( response){  
-               $info.html("<h1>" + title + "</h1>" + response);
+               $info.html("<h1>" + title + "</h1>" + response.slice(0, -1));
                 //alert(response)
             });
         });
