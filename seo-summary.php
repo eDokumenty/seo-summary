9390133ea5e4995dd2cca8a8d5d5273eb773fdd6
klauuduu98 <klaudiawasilewska98@gmail.com>
2016-08-24 08:36:06 +0200

    add_action in new file

--- seo-summary.php
+++ seo-summary.php
@@ -79,74 +79,7 @@ function queue_my_admin_scripts() {
     
 }
 
-
-add_action('wp_ajax_get_text', function() {
-    $post = $_POST['post_name'];
-    
-    $seo = new SEOSummaryLinksManager();
-    $url = $seo->getUrl($post);
-    
-    $text = CrawlPages::getTextSimplePage($url);
-    
-    ?>
-<pre>
-    <?php echo $text; ?>
-</pre>
-    <?php
-    return true;
-});
-
-add_action('wp_ajax_get_inLink', function() {
-   $post = $_POST['post_name']; 
-   $seo = new SEOSummaryLinksManager();
-   $data = $seo->getAllLinkOnPage($post);
-   ?>
-
-    <?php
-        $lp = 1;
-        echo '<table class="wp-list-table widefat striped posts">';
-        echo '<thead>'
-                . '<th style="text-align: center;">L.p</th>'
-                . '<th>URL</th>'
-                . '<th>Ile razy</th>'
-            . '</thead>';
-        foreach ($data as $row){
-            echo '<tr>';
-            echo '<td style="width:50px;text-align: center;">'.$lp++.'</td>';
-            echo '<td>'.$row['url'].'</td>';
-            echo '<td>'.$row['replay'].'</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-    return true;
-});
-
-add_action('wp_ajax_get_outLink', function(){
-    $post = $_POST['post_name'];
-    
-    $seo = new SEOSummaryLinksManager();
-    
-    $data = $seo->getAllLinkCallToPage($post);
-   ?>
-
-<?php
-        $lp = 1;
-        echo '<table class="wp-list-table widefat striped posts">';
-        echo '<thead>'
-                . '<th style="text-align: center;">L.p</th>'
-                . '<th>URL</th>'
-                . '<th>Ile razy</th>'
-            . '</thead>';
-        foreach ($data as $row){
-            echo '<tr>';
-            echo '<td style="width:50px;text-align: center;">'.$lp++.'</td>';
-            echo '<td>'.$row['url'].'</td>';
-            echo '<td>'.$row['replay'].'</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-    return true;
-});
+require_once PLUGIN_SEO_DIR.'/wp_ajax.php';;
 
 /*
  * Add plugin to the Wordpress menu
